d9ab63e18dfee6b28f6220d1c730e03916142957
brendef <brendan.defaria@gmail.com>
2024-06-15 11:53:39 +0200

    add dev instagram auth

--- app/routes.py
+++ app/routes.py
@@ -87,6 +87,27 @@ def login_template(request: Request):
 @router.get("/home")
 def home_template(request: Request):
 
+    ENV = os.getenv("ENV")
+
+    if ENV == "DEV":
+        context = {"instagram_auth": True}
+        context["env"] = ENV
+
+        devLongToken = Instagram.INSTAGRAM_DEV_TOKEN
+
+        response = templates.TemplateResponse(
+            request=request, name="pages/home.html", context=context
+        )
+        response.set_cookie(
+            key=LONG_TOKEN,
+            value=devLongToken,
+            expires=datetime.now(timezone.utc) + timedelta(seconds=86400 * 399),
+            secure=True,
+            httponly=True,
+        )
+
+        return response
+
     context = {"instagram_auth": False}
 
     # get the cookie from the request
@@ -131,12 +152,33 @@ def feed_htmx(request: Request, nextUrl: str):
     for image in feed["data"]:
 
         if image["media_type"] == "CAROUSEL_ALBUM":
-            pass
+            htmlResponse += f"""
+             <div id="{image["id"]}" hx-get="/get-carousel-album/{image["id"]}" class="image border border-gray-200"
+                hx-swap="outerHTML">
+                <p class="carousel-album-text absolute bg-black text-white px-2">album</p>
+                <img class="thumbnail carousel" src=" {image["media_url"]}" alt="" />
+            </div>
+            <div id="carousel-loader-{image["id"]}" class="loader-class" hidden>
+                <div role="status">
+                    <svg class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor" />
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill" />
+                    </svg>
+                    <span class="sr-only">Loading...</span>
+                </div>
+                <p>Loading album...</p>
+            </div>
+            """
 
         if image["media_type"] == "VIDEO":
 
             htmlResponse += f"""
-            <div id="{{ image.id }}" class="image border border-gray-200">
+            <div id="{ image["id"] }" class="image border border-gray-200">
                 <video id="{image["id"]}" class="thumbnail aspect-square" width="500" height="100">
                     <source src="{image["media_url"]}" type="video/mp4">
                     Your browser does not support the video tag.
@@ -147,7 +189,8 @@ def feed_htmx(request: Request, nextUrl: str):
                 document.getElementById('select-all-box').checked = false;
             </script>
         """
-        else:
+
+        if image["media_type"] == "IMAGE":
             htmlResponse += f"""
             <div id="{image["id"]}" class="image border border-gray-200"> <img class="thumbnail" src="{image["media_url"]}" alt="" /> </div>
         """
@@ -198,23 +241,43 @@ def carousel_album_htmx(request: Request, media_id: str):
         return HTMLResponse(content="An error occured")
 
     htmlResponse = ""
-    for pos, child in enumerate(media["children"]["data"]):
+    for child in media["children"]["data"]:
         id = child["id"]
         childImage = instagram.get_child_media(id)
 
-        addBorder = ""
-        if pos == 0:
-            addBorder = "image-selected-border"
-
         htmlResponse += f"""
             <div id="{childImage["id"]}" class="image border border-gray-200">
-                <img class="thumbnail {addBorder}" src="{childImage["media_url"]}" alt="" />
+                <img class="thumbnail" src="{childImage["media_url"]}" alt="" />
             </div>
 
             <script>
                 document.getElementById("{childImage["id"]}").addEventListener('click', (e) => {{
                     selectImage(e.srcElement.src, e.target, "{childImage["id"]}")
+
+                    e.addEventListener('htmx:beforeRequest', function (evt) {{
+                        let id = evt.srcElement.id
+
+                        const loadingIndicator = document.querySelector(`#carousel-loader-${childImage["id"]}`);
+                        let src = document.getElementById(evt.srcElement.id).querySelector(".thumbnail").src
+                        // remove src from selectedImages array
+                        selectedImages = selectedImages.filter(item => item != src)
+
+                        if (loadingIndicator) {{
+                            loadingIndicator.style.display = 'block';
+                        }}
+
+                    }});
+
+                    e.addEventListener('htmx:afterRequest', function (evt) {{
+                        let id = evt.srcElement.id
+                        const loadingIndicator = document.querySelector(`#carousel-loader-${childImage["id"]}`);
+                        if (loadingIndicator) {{
+                            loadingIndicator.style.display = 'none';
+                        }}
+
+                }});
                 }})
+                
             </script>
         """
 
